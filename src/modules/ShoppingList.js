2d996f981af78dba1c02db2e6cb61fb1bc6f8fa7
Hasini Wijerathna <hasininwj@gmail.com>
2019-09-03 17:17:51 +0800

    Added shopping lost and view screens

---
+++ src/modules/ShoppingList.js
@@ -0,0 +1,50 @@
+import * as React from "react";
+import {
+    Text,
+    View,
+    Button,
+    StyleSheet
+} from 'react-native';
+import { observer } from "mobx-react-lite";
+import { RouterStoreContext } from "../stores/RoutesStore";
+
+
+export const ShoppingList = observer(() => {
+    const routerStore = React.useContext(RouterStoreContext);
+    return (
+        <View style={styles.container}>
+            <Text>Shopping list</Text>
+            <Button style={styles.logo} title="Go to" onPress={() => routerStore.screen = 'Venues'} />
+        </View>
+    );
+});
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    logo: {
+        width: 300,
+        height: 300,
+    },
+    title: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    button: {
+        borderRadius: 3,
+        padding: 20,
+        marginVertical: 10,
+        marginTop: 100,
+        backgroundColor: '#e01b7a',
+    },
+    buttonText: {
+        color: '#fff',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+});
---
+++ src/modules/Venues.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+import {
+    Text,
+    View,
+} from 'react-native';
+
+
+export const Venues = () => {
+    return (
+        <View>
+            <Text>
+                Venues Page
+            </Text>
+        </View>
+    );
+}
