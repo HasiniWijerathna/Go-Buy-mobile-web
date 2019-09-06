6e6d32affec9553e8fe778801ac52e39929ede37
Hasini Wijerathna <hasini.wijerathna@quantuminventions.com>
2019-09-06 14:21:30 +0800

    Code refactoring

--- src/modules/Home.js
+++ src/modules/Home.js
@@ -1,46 +1,37 @@
 import * as React from "react";
 import {
-    Text,
-    View,
-    Button,
-    StyleSheet,
-    ScrollView,
-    ActivityIndicator
-} from 'react-native';
+  Text,
+  View,
+  Button,
+  StyleSheet,
+  ScrollView,
+  ActivityIndicator
+} from "react-native";
 import { observer } from "mobx-react-lite";
 import { RouterStoreContext } from "../stores/RoutesStore";
 import { ShoppingCard } from "../viewElements/ShoppingCard";
 
-
 export const Home = observer(() => {
-    const routerStore = React.useContext(RouterStoreContext);
-    return (
-        <ScrollView
-        noSpacer={true}
-        noScroll={true}
-        style={styles.container}
-      >
-       <ActivityIndicator
-            style={[styles.centering, styles.gray]}
-            color="#ff8179"
-            size="large"
-/>
-      </ScrollView>
-    );
+  const routerStore = React.useContext(RouterStoreContext);
+  return (
+    <ScrollView noSpacer={true} noScroll={true} style={styles.container}>
+      <ActivityIndicator
+        style={[styles.centering, styles.gray]}
+        color="#ff8179"
+        size="large"
+      />
+    </ScrollView>
+  );
 });
 
-   
 const styles = StyleSheet.create({
-    container: {
-      backgroundColor: "whitesmoke"
-    },
-    centering: {
-      alignItems: "center",
-      justifyContent: "center",
-      padding: 8,
-      height: '100vh'
-    },
-  });
-  
-
-
+  container: {
+    backgroundColor: "whitesmoke"
+  },
+  centering: {
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+    height: "100vh"
+  }
+});
