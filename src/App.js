971c2fabc19d737eff479d58303e7f909089eec2
Hasini Wijerathna <hasini.wijerathna@quantuminventions.com>
2019-09-06 14:20:33 +0800

    Integrated react push notification

--- src/App.js
+++ src/App.js
@@ -1,38 +1,18 @@
-import React, { Component } from 'react';
-import {
-  Platform,
-  Animated,
-  Easing,
-} from 'react-native';
-import Router from './config/Router';
+import React, { Component } from "react";
+import { Platform, Animated } from "react-native";
+import Router from "./config/Router";
+import PushNotification from "./services/PushNotification";
 
 class App extends Component {
-  state = {
-    spinValue: new Animated.Value(0),
-  }
-
-  onClick = () => {
-    const wasRotated = this.state.spinValue._value === 1;
-    Animated.timing(
-      this.state.spinValue,
-      {
-        toValue: wasRotated ? 0 : 1,
-        duration: 250,
-        easing: Easing.linear
-      }
-    ).start()
-  }
 
   render() {
-    return (
-      <Router />
-    );
+    return <Router />;
   }
 }
 
 let hotWrapper = () => () => App;
-if (Platform.OS === 'web') {
-  const { hot } = require('react-hot-loader');
+if (Platform.OS === "web") {
+  const { hot } = require("react-hot-loader");
   hotWrapper = hot;
 }
 export default hotWrapper(module)(App);
