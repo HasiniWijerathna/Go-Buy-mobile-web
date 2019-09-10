e1715a3157a657dac865134107cb60c63f2b2d8e
Hasini Wijerathna <hasini.wijerathna@quantuminventions.com>
2019-09-10 16:08:24 +0800

    Updated userstore

--- src/stores/UserStore.js
+++ src/stores/UserStore.js
@@ -2,7 +2,22 @@ import { observable } from "mobx";
 import { CreateContext } from "react";
 
 class UserStore {
-    @observable geoLocation = {};
+  @observable geoLocation = { latitude: "", longitude: "" };
+  @observable shoppingList = [];
+  @observable isLoading = true;
+
+  addItemstoShoppingStore = shoppingItem => {
+    this.isLoading = true;
+    this.shoppingList.push(shoppingItem);
+    this.isLoading = false;
+    return shoppingItem;
+  };
+
+  addGeoLocationStore = coordinates => {
+    this.isLoading = true;
+    this.geoLocation = coordinates;
+    this.isLoading = false;
+  };
 }
 
 export const ShoppingStoreContext = CreateContext(new UserStore());
