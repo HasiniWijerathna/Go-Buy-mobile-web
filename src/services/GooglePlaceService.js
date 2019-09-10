8f2cc8398d9c1cda8612ff47faa8f6f533476595
Hasini Wijerathna <hasini.wijerathna@quantuminventions.com>
2019-09-10 17:32:25 +0800

    Added fetch geo location

--- src/services/GooglePlaceService.js
+++ src/services/GooglePlaceService.js
@@ -1,21 +1,28 @@
-import GOOGLE_API_KEY from '../config/AppConst';
-import Geolocation from '@react-native-community/geolocation';
+import GOOGLE_API_KEY from "../config/AppConst";
+import Geolocation from "@react-native-community/geolocation";
+import { ShopperStoreContext } from "../stores/ShopperStore";
 
 /**
  * Fetch the available places for the identified category
- * @param {string} placeType 
+ * @param {string} placeType
  */
-const fetchNearPlaces = placeType => {
-
-}
+const fetchNearPlaces = placeType => {};
 
 /**
- * Fetches the users current location 
+ * Fetches the users current location
+ *
+ * When fetchCurrentLocation gets triggered, 
+ * it uses the getCurrentPosition funciton to pass the coordinates to the shopper store
  */
 const fetchCurrentLocation = () => {
+  const shopperStore = useContext(ShopperStoreContext);
+
+  Geolocation.getCurrentPosition(info => {
     const coordinates = {
-        latitude : '',
-        longitude: ''
-    }
-    Geolocation.getCurrentPosition(info => console.log(info.coords))
-}
+      latitude: info.coords.latitude,
+      longitude: info.coords.longitude
+    };
+
+    shopperStore.addGeoLocationStore(coordinates);
+  });
+};
